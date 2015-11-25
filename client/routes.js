584077f2ba5ab424baaf75c83a2d0be102b3a4e3
david <davidvoler@gmail.com>
2015-11-25 17:10:03 +0200

    router is setup

--- client/routes.js
+++ client/routes.js
@@ -4,10 +4,43 @@ Router.configure({
 });
 
 Router.route('/', {name: 'home', controller: 'MainController'});
+Router.route('/angular1', {name: 'angular1', controller: 'Angular1Controller'});
+Router.route('/angular2', {name: 'angular2', controller: 'Angular2Controller'});
+Router.route('/react', {name: 'react', controller: 'ReactController'});
 
 MainController = RouteController.extend({
   action: function() {
   	this.render('home', {
+	    data: function () {
+	      return { posts: ['post red', 'post blue'] }
+	    }
+  	});
+  }
+});
+
+Angular1Controller = RouteController.extend({
+  action: function() {
+  	this.render('angular1', {
+	    data: function () {
+	      return { posts: ['post red', 'post blue'] }
+	    }
+  	});
+  }
+});
+
+Angular2Controller = RouteController.extend({
+  action: function() {
+  	this.render('angular2', {
+	    data: function () {
+	      return { posts: ['post red', 'post blue'] }
+	    }
+  	});
+  }
+});
+
+ReactController = RouteController.extend({
+  action: function() {
+  	this.render('react', {
 	    data: function () {
 	      return { posts: ['post red', 'post blue'] }
 	    }
